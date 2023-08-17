fc5274595b55da56092f2ab7d7efc05e701c1d28
davidsstefano <50884100+davidsstefano@users.noreply.github.com>
2023-08-17 10:31:45 -0300

    organizaçao de rotas

--- src/controllers/controllers_favoritos.js
+++ src/controllers/controllers_favoritos.js
@@ -3,26 +3,11 @@ import db from "../config/database.js";
 
 const controllerFavoritos = Router();
 
-controllerFavoritos.get("/generos", function (request, response) {
-  let sql = "SELECT * FROM generos";
-  db.query(sql, function (err, result) {
-    if (err) {
-      return response.status(500).send(err);
-    } else {
-      return response.status(200).json(result);
-    }
-  });
-});
-controllerFavoritos.get("/generos/:id_genero", function (request, response) {
-  let sql = "SELECT * FROM generos WHERE id_genero = ? ";
-  db.query(sql, [request.params.id_genero], function (err, result) {
-    if (err) {
-      return response.status(500).send(err);
-    } else {
-      return response.status(200).json(result);
-    }
-  });
-});
+
+
+
+//################################## ROTAS FILMES FAVORITOS   #################################//
+
 controllerFavoritos.get("/filmes_favoritos", function (request, response) {
   let sql = "SELECT * FROM favoritos_filmes";
   db.query(sql, function (err, result) {
@@ -54,76 +39,6 @@ controllerFavoritos.get(
     });
   }
 );
-
-controllerFavoritos.get("/generos_favoritos", function (request, response) {
-  let sql = "SELECT * FROM favoritos_generos";
-  db.query(sql, function (err, result) {
-    if (err) {
-      return response.status(500).send(err);
-    } else {
-      return response.status(200).json(result);
-    }
-  });
-});
-
-controllerFavoritos.get(
-  "/generos_favoritos/:user_id",
-  function (request, response) {
-    let sql = "SELECT * FROM favoritos_generos WHERE id_usuario_fg = ?";
-    db.query(sql, [request.params.user_id], function (err, result) {
-      if (err) {
-        return response.status(500).send(err);
-      } else {
-        if (result.length > 0) {
-          return response.status(200).json(result);
-        } else {
-          return response
-            .status(404)
-            .json({ message: "Usuário não encontrado" });
-        }
-      }
-    });
-  }
-);
-
-controllerFavoritos.post("/generos_favoritos/cadastro", async function (request, response) {
-  try {
-    const id_usuario_fg = request.body.id_usuario_fg;
-    const id_genero_fg = request.body.id_genero_fg;
-    const currentDate = new Date();
-    const status = 1;
-
-    const checkQuery = "SELECT * FROM favoritos_generos WHERE id_usuario_fg = ? AND id_genero_fg = ?";
-    db.query(checkQuery, [id_usuario_fg, id_genero_fg], function (err, results) {
-      if (err) {
-        return response.status(500).send(err);
-      }
-
-      if (results.length > 0) {
-       
-        const updateQuery = "UPDATE favoritos_generos SET status_ativacao_fg = ? WHERE id_usuario_fg = ? AND id_genero_fg = ?";
-        db.query(updateQuery, [status, id_usuario_fg, id_genero_fg], function (err, updateResult) {
-          if (err) {
-            return response.status(500).send(err);
-          }
-          return response.status(200).json({ message: "Updated status." });
-        });
-      } else {
-  
-        const insertQuery = "INSERT INTO favoritos_generos (id_usuario_fg, id_genero_fg, data_atualizacao_fg, status_ativacao_fg) VALUES (?, ?, ?, ?)";
-        db.query(insertQuery, [id_usuario_fg, id_genero_fg, currentDate, status], function (err, insertResult) {
-          if (err) {
-            return response.status(500).send(err);
-          }
-          return response.status(201).json({ id_cadastro: insertResult.insertId });
-        });
-      }
-    });
-  } catch (error) {
-    console.error("Error during user registration:", error);
-    return response.status(500).json({ message: "Internal Server Error" });
-  }
-});
 
 
 controllerFavoritos.post("/filmes_favoritos/cadastro", async function (request, response) {
@@ -229,6 +144,99 @@ controllerFavoritos.delete(
   }
 );
 
+
+//################################## ROTAS GENEROS FAVORITOS  #################################//
+controllerFavoritos.get("/generos", function (request, response) {
+  let sql = "SELECT * FROM generos";
+  db.query(sql, function (err, result) {
+    if (err) {
+      return response.status(500).send(err);
+    } else {
+      return response.status(200).json(result);
+    }
+  });
+});
+controllerFavoritos.get("/generos/:id_genero", function (request, response) {
+  let sql = "SELECT * FROM generos WHERE id_genero = ? ";
+  db.query(sql, [request.params.id_genero], function (err, result) {
+    if (err) {
+      return response.status(500).send(err);
+    } else {
+      return response.status(200).json(result);
+    }
+  });
+});
+
+controllerFavoritos.get("/generos_favoritos", function (request, response) {
+  let sql = "SELECT * FROM favoritos_generos";
+  db.query(sql, function (err, result) {
+    if (err) {
+      return response.status(500).send(err);
+    } else {
+      return response.status(200).json(result);
+    }
+  });
+});
+
+controllerFavoritos.get(
+  "/generos_favoritos/:user_id",
+  function (request, response) {
+    let sql = "SELECT * FROM favoritos_generos WHERE id_usuario_fg = ?";
+    db.query(sql, [request.params.user_id], function (err, result) {
+      if (err) {
+        return response.status(500).send(err);
+      } else {
+        if (result.length > 0) {
+          return response.status(200).json(result);
+        } else {
+          return response
+            .status(404)
+            .json({ message: "Usuário não encontrado" });
+        }
+      }
+    });
+  }
+);
+
+controllerFavoritos.post("/generos_favoritos/cadastro", async function (request, response) {
+  try {
+    const id_usuario_fg = request.body.id_usuario_fg;
+    const id_genero_fg = request.body.id_genero_fg;
+    const currentDate = new Date();
+    const status = 1;
+
+    const checkQuery = "SELECT * FROM favoritos_generos WHERE id_usuario_fg = ? AND id_genero_fg = ?";
+    db.query(checkQuery, [id_usuario_fg, id_genero_fg], function (err, results) {
+      if (err) {
+        return response.status(500).send(err);
+      }
+
+      if (results.length > 0) {
+       
+        const updateQuery = "UPDATE favoritos_generos SET status_ativacao_fg = ? WHERE id_usuario_fg = ? AND id_genero_fg = ?";
+        db.query(updateQuery, [status, id_usuario_fg, id_genero_fg], function (err, updateResult) {
+          if (err) {
+            return response.status(500).send(err);
+          }
+          return response.status(200).json({ message: "Updated status." });
+        });
+      } else {
+  
+        const insertQuery = "INSERT INTO favoritos_generos (id_usuario_fg, id_genero_fg, data_atualizacao_fg, status_ativacao_fg) VALUES (?, ?, ?, ?)";
+        db.query(insertQuery, [id_usuario_fg, id_genero_fg, currentDate, status], function (err, insertResult) {
+          if (err) {
+            return response.status(500).send(err);
+          }
+          return response.status(201).json({ id_cadastro: insertResult.insertId });
+        });
+      }
+    });
+  } catch (error) {
+    console.error("Error during user registration:", error);
+    return response.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
 controllerFavoritos.delete(
   "/generos_favoritos/:id_favorito_generos",
   async function (request, response) {
@@ -258,5 +266,6 @@ controllerFavoritos.delete(
     }
   }
 );
+
 
 export default controllerFavoritos;
