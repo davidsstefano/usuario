5360b629523b9b276e3e0a48d246203816b1d628
davidsstefano <50884100+davidsstefano@users.noreply.github.com>
2023-08-23 11:31:43 -0300

    cronos

--- src/controllers/controllers_killdb.js
+++ src/controllers/controllers_killdb.js
@@ -1,20 +1,47 @@
 import { Router } from "express";
 import db from "../config/database.js";
 
+
 const controllerkilldb = Router();
 
+
 controllerkilldb.get("/kill", function (request, response) {
-     let sql = "SELECT CONCAT('KILL ',id,';') AS run_this FROM information_schema.processlist WHERE user='hiddenwolf'";
      
-     db.query(sql, function (err, result) {
+     let checkConnectionsSql = "SELECT id FROM information_schema.processlist WHERE user='hiddenwolf'";
+     
+     db.query(checkConnectionsSql, function (err, result) {
        if (err) {
          return response.status(500).send(err);
        } else {
-         return response.status(200).json(result);
+         if (result.length > 2) {
+           let killedConnections = [];
+   
+           for (let i = 0; i < result.length; i++) {
+             let killQuery = `KILL ${result[i].id}`;
+   
+             db.query(killQuery, function (err, killResult) {
+               if (err) {
+                 return response.status(500).send(err);
+               }
+             });
+   
+             killedConnections.push(result[i].id);
+           }
+           
+           return response.status(200).json({
+             message: "Connections killed.",
+             killedConnections: killedConnections
+           });
+         } else {
+           return response.status(200).json({
+             message: "No connections to kill.",
+             activeConnections: result.length
+           });
+         }
        }
      });
    });
-
-
+   
+   
 
 export default controllerkilldb;
