66576d247a946aa1067285dfa226de14c51a2075
davidsstefano <50884100+davidsstefano@users.noreply.github.com>
2023-08-24 11:59:32 -0300

    indicacoes

--- src/controllers/controllers_indicacao.js
+++ src/controllers/controllers_indicacao.js
@@ -16,5 +16,55 @@ controllerIndicacao.get("/indicacoes", function (request, response) {
    });
 
 
+   controllerIndicacao.post("/indicacao", async (request, response) => {
+    try {
+      const meuid = request.body.meuid;
+      const id_indicado = request.body.id_indicado;
+      const id_filme_fm = request.body.id_filme_fm;
+      const currentDate = new Date();
+  
+      // Verifica se ambos os usuários existem
+      const checkQuery = `
+        SELECT 
+          CASE 
+            WHEN (SELECT COUNT(*) FROM usuarios WHERE id_user IN (?, ?)) = 2 THEN 1
+            ELSE 0
+          END AS both_users_present;
+      `;
+      const checkValues = [meuid, id_indicado];
+      
+      db.query(checkQuery, checkValues, (err, results) => {
+        if (err) {
+          console.error("Erro durante a verificação da existência do usuário:", err);
+          return response.status(500).json({ mensagem: "Erro Interno do Servidor" });
+        }
+        const ambosUsuariosPresentes = results[0].both_users_present;
+  
+        if (!ambosUsuariosPresentes) {
+          return response.status(400).json({ mensagem: "Um ou ambos os usuários não existem" });
+        }
+  
+        // Insere a recomendação
+        const insertQuery = `
+          INSERT INTO indicacoes (id_user_ind, id_user_rec, id_filme, data_indicacao)
+          VALUES (?, ?, ?, ?);
+        `;
+        const insertValues = [meuid, id_indicado, id_filme_fm, currentDate];
+        
+        db.query(insertQuery, insertValues, (insertErr, insertResult) => {
+          if (insertErr) {
+            console.error("Erro durante a inserção da recomendação:", insertErr);
+            return response.status(500).json({ mensagem: "Erro Interno do Servidor" });
+          }
+          return response.status(201).json({ id_cadastro: insertResult.insertId });
+        });
+      });
+    } catch (error) {
+      console.error("Erro durante a criação da recomendação:", error);
+      return response.status(500).json({ mensagem: "Erro Interno do Servidor" });
+    }
+  });
+  
+
 
 export default controllerIndicacao;
